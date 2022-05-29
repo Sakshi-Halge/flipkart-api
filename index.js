b2c8fddc7ae4ec6bacfd08d51772958f941abfe4
Sakshi-Halge <sakshihalge2000@gmail.com>
2022-05-29 09:21:50 +0530

    api

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const port_number = process.env.PORT || 8754;
+const cors = require('cors');
 //Import routes
 const main_cat_route = require('./routes/display_main');
 const sub_cat_route = require('./routes/display_sub');
@@ -17,6 +18,7 @@ mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () =>
   console.log("Connected to db!")
 );
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended : true}))
 
